dc5055035b2fcefadd595583174f3ca80eff985c
Patryk Olechno <patrykoolechno@gmail.com>
2019-10-29 11:13:26 +0100

    Strona proste działania i strona głowna

---
+++ strona2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Potęgowanie</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <a href="index.htm"><img src="baner.jpg" alt="Działania matematyczne"></a>
+      </header>
+      <nav>
+        Menu
+        <ul>
+          <li>- <a href="strona1.htm">Proste działania</a></li>
+          <li>- <a href="strona2.php">Potęgowanie</a></li>
+        </ul>
+      </nav>
+      <main>
+        <h1>Potęgowanie</h1>
+      </main>
+      <footer></footer>
+  </div>
+  </body>
+</html>
